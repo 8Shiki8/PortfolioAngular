3f966481980df431b8ca464bf42d41109c8b52b5
Shiki <jigen99@hotmail.com>
2024-06-19 01:08:00 -0300

    2 remover modulos innecesarios

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InicioComponent } from './inicio/inicio.component';
-import { HomeRoutingModule } from './home-routing.module';
+
 import { SharedModule } from '../shared/shared.module';
 import { MatButtonModule } from '@angular/material/button';
 
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-    imports: [CommonModule, HomeRoutingModule, FormsModule, SharedModule, MatButtonModule, InicioComponent],
+    imports: [CommonModule, FormsModule, SharedModule, MatButtonModule, InicioComponent],
     exports: [InicioComponent],
 })
 export class HomeModule {}
