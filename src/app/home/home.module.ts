5b8dce50fce323e3ebb4076b2e9a984c3570d67b
Shiki <jigen99@hotmail.com>
2024-06-19 20:13:36 -0300

    removidos modules innecesarios

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { InicioComponent } from './inicio/inicio.component';
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { InicioComponent } from './inicio/inicio.component';
 
-import { SharedModule } from '../shared/shared.module';
-import { MatButtonModule } from '@angular/material/button';
+// import { SharedModule } from '../shared/shared.module';
+// import { MatButtonModule } from '@angular/material/button';
 
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
 
-@NgModule({
-    imports: [CommonModule, FormsModule, SharedModule, MatButtonModule, InicioComponent],
-    exports: [InicioComponent],
-})
-export class HomeModule {}
+// @NgModule({
+//     imports: [CommonModule, FormsModule, SharedModule, MatButtonModule, InicioComponent],
+//     exports: [InicioComponent],
+// })
+// export class HomeModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,55 +1,55 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+// import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { CommonModule } from '@angular/common';
-import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LayoutComponent } from './layout/layout.component';
-import { RouterModule } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { TiendaModule } from '../tienda/tienda.module';
-import { DialogComponent } from './dialog/dialog.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatBadgeModule } from '@angular/material/badge';
-import { FormsModule } from '@angular/forms';
-import { TableComponent } from './table/table.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatInputModule } from '@angular/material/input';
-import { CarritoComponent } from './navbar/carrito/carrito.component';
+// import { CommonModule } from '@angular/common';
+// import { FooterComponent } from './footer/footer.component';
+// import { NavbarComponent } from './navbar/navbar.component';
+// import { LayoutComponent } from './layout/layout.component';
+// import { RouterModule } from '@angular/router';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatToolbarModule } from '@angular/material/toolbar';
+// import { MatButtonModule } from '@angular/material/button';
+// import { TiendaModule } from '../tienda/tienda.module';
+// import { DialogComponent } from './dialog/dialog.component';
+// import { MatDialogModule } from '@angular/material/dialog';
+// import { MatTabsModule } from '@angular/material/tabs';
+// import { MatChipsModule } from '@angular/material/chips';
+// import { MatBadgeModule } from '@angular/material/badge';
+// import { FormsModule } from '@angular/forms';
+// import { TableComponent } from './table/table.component';
+// import { MatTableModule } from '@angular/material/table';
+// import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+// import { MatInputModule } from '@angular/material/input';
+// import { CarritoComponent } from './navbar/carrito/carrito.component';
 
-@NgModule({
-    imports: [
-        CommonModule,
-        RouterModule,
-        MatIconModule,
-        MatToolbarModule,
-        MatButtonModule,
-        TiendaModule,
-        MatDialogModule,
-        MatTabsModule,
-        MatChipsModule,
-        MatBadgeModule,
-        FormsModule,
-        MatTableModule,
-        MatSlideToggleModule,
-        MatInputModule,
-        DialogComponent,
-        FooterComponent,
-        NavbarComponent,
-        LayoutComponent,
-        TableComponent,
-        CarritoComponent,
-    ],
-    exports: [
-        DialogComponent,
-        FooterComponent,
-        NavbarComponent,
-        LayoutComponent,
-        TableComponent,
-    ],
-})
-export class SharedModule {}
+// @NgModule({
+//     imports: [
+//         CommonModule,
+//         RouterModule,
+//         MatIconModule,
+//         MatToolbarModule,
+//         MatButtonModule,
+//         TiendaModule,
+//         MatDialogModule,
+//         MatTabsModule,
+//         MatChipsModule,
+//         MatBadgeModule,
+//         FormsModule,
+//         MatTableModule,
+//         MatSlideToggleModule,
+//         MatInputModule,
+//         DialogComponent,
+//         FooterComponent,
+//         NavbarComponent,
+//         LayoutComponent,
+//         TableComponent,
+//         CarritoComponent,
+//     ],
+//     exports: [
+//         DialogComponent,
+//         FooterComponent,
+//         NavbarComponent,
+//         LayoutComponent,
+//         TableComponent,
+//     ],
+// })
+// export class SharedModule {}
