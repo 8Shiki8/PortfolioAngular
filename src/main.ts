469763dd8988aa5f9fc752ecdc0212e655695d90
Shiki <jigen99@hotmail.com>
2024-06-19 01:15:18 -0300

    3 cambios del bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { SharedModule } from './app/shared/shared.module';
+import { TiendaModule } from './app/tienda/tienda.module';
+import { HomeModule } from './app/home/home.module';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, HomeModule, TiendaModule, SharedModule),
+        provideAnimations()
+    ]
+})
   .catch(err => console.error(err));
